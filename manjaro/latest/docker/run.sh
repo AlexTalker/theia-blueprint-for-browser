615817f1188b48d593c3a8bf23fdafc1164f22bb
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2022-07-02 15:36:40 +0300

    Update run.sh
    Put artifacts back in /data

--- manjaro/latest/docker/run.sh
+++ manjaro/latest/docker/run.sh
@@ -10,3 +10,7 @@ cp /data/common/{theia-blueprint-for-browser.sh,package.json} .
 cp /data/manjaro/latest/PKGBUILD .
 
 makepkg --syncdeps --noconfirm
+
+mkdir -p /data/output
+
+cp *.zst /data/output
