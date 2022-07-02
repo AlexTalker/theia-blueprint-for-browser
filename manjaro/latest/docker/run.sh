046706558e7bcba40a13886953e62bbafdd40e8e
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2022-07-02 22:30:25 +0300

    Update run.sh
    Fix typo

--- manjaro/latest/docker/run.sh
+++ manjaro/latest/docker/run.sh
@@ -6,7 +6,7 @@ BUILDDIR="$(mktemp -d)"
 
 cd "${BUILDDIR}"
 
-cp /data/common/{theia-blueprint-for-browser.{sh,service},package.json} .
+cp /data/common/{theia-blueprint-for-browser{.sh,@.service},package.json} .
 cp /data/manjaro/latest/PKGBUILD .
 
 makepkg --syncdeps --noconfirm
